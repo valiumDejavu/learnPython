983dc50805a32d3af0629b8b2f3661e03a9cc123
VSW\jiafei.duan <jeffer_duan@hotmail.com>
2017-05-12 10:39:56 +0200

    no message

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
+#from flask import Flask
+#app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return 'Hello, World!'
+#@app.route('/')
+#def hello_world():
+ # return 'Hello, World!'
 
-if __name__ == '__main__':
-  app.run()
+#if __name__ == '__main__':
+ # app.run()
+
+#!flask/bin/python
+from app import app
+app.run(debug=True)
 
