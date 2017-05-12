111691c46bb20db17ab8c7c32a8ced60384fd635
VSW\jiafei.duan <jeffer_duan@hotmail.com>
2017-05-12 12:04:28 +0200

    added some content

--- app/views.py
+++ app/views.py
@@ -4,5 +4,15 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Miguel'}  # fake user
-    return render_template('index.html',title='Home',user=user)
+    user = {'nickname': 'Jiafei'}  # fake user
+    posts = [  # fake array of posts
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html',title='Home',user=user, posts=posts)
