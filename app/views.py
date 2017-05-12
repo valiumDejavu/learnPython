79c8c5bbe08c186a03c84c67bd70d5768c027cd9
VSW\jiafei.duan <jeffer_duan@hotmail.com>
2017-05-12 14:15:39 +0200

    setup login forms

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
+from flask import flash
+from flask import redirect
 from app import app
 from flask import render_template
-
+from .forms import LoginForm
+'''
 @app.route('/')
 @app.route('/index')
 def index():
@@ -16,3 +19,15 @@ def index():
         }
     ]
     return render_template('index.html',title='Home',user=user, posts=posts)
+'''
+@app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form)
